e99327ee33e7ce53e2401ed5056ada432aee74e5
Vyacheslav Nechepurenko <masterdog8@mail.ru>
2023-04-02 13:45:45 +0300

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.io.IOException;
+
+public class Main {
+    private static final int SERVER_SOCKET = 9999;
+
+    public static void main(String[] args) {
+
+    }
+}
